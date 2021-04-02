f473ef9b6d1292a5835b45f65d7d2ccf2086ec78
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-04-02 23:39:22 +0200

    feat: compress payload using brotli

--- src/index.js
+++ src/index.js
@@ -2,17 +2,34 @@
 
 const mql = require('@microlink/mql')
 
-const microlink = (code, mqlOpts, gotOpts) => (url, opts) =>
-  mql(
-    url,
-    {
-      function: code.toString(),
-      meta: false,
-      ...mqlOpts,
-      ...opts
-    },
-    gotOpts
-  ).then(({ data }) => data.function)
+const { escape } = require('base64-url')
+const { promisify } = require('util')
+const zlib = require('zlib')
+
+const brotliCompress = promisify(zlib.brotliCompress)
+
+const microlink = (code, { compress, ...mqlOpts }, gotOpts) => {
+  const codeFn = compress
+    ? brotliCompress(code.toString()).then(
+        data => `br#${escape(data.toString('base64'))}`
+      )
+    : Promise.resolve(code.toString())
+
+  return async (url, opts) => {
+    const { data } = await mql(
+      url,
+      {
+        function: await codeFn,
+        meta: false,
+        ...mqlOpts,
+        ...opts
+      },
+      gotOpts
+    )
+
+    return data.function
+  }
+}
 
 microlink.version = '__VERSION__'
 microlink.mql = mql
--- test/index.js
+++ test/index.js
@@ -2,12 +2,12 @@
 
 const test = require('ava')
 
-const microlinkFunction = require('..')
+const microlink = require('..')
 
 test('interact with the page', async t => {
   const getTitle = ({ page }) => page.title()
 
-  const myFn = microlinkFunction(getTitle)
+  const myFn = microlink(getTitle)
 
   const result = await myFn('https://google.com')
 
@@ -21,7 +21,7 @@ test('interact with the page', async t => {
 test('interact with the response', async t => {
   const getTitle = ({ response }) => response.status()
 
-  const myFn = microlinkFunction(getTitle)
+  const myFn = microlink(getTitle)
 
   const result = await myFn('https://google.com')
 
@@ -35,7 +35,7 @@ test('interact with the response', async t => {
 test('interact with the query', async t => {
   const getTitle = ({ query }) => query.greetings
 
-  const myFn = microlinkFunction(getTitle)
+  const myFn = microlink(getTitle)
 
   const result = await myFn('https://google.com', { greetings: 'hello world' })
 
